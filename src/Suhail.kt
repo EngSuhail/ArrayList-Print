2b4c28a406ca5c6b10334fe9e005d5943ed68607
EngSuhail <73076012+EngSuhail@users.noreply.github.com>
2020-11-09 20:55:52 +0200

    ArrayList-Print-AppsSquare

---
+++ src/Suhail.kt
@@ -0,0 +1,22 @@
+fun main() {
+
+    val engineers = ArrayList<String>()
+
+    engineers.add("Suhail")
+    engineers.add("Adel")
+    engineers.add("Mostafa")
+    engineers.add("Ibrahim")
+    engineers.add("Mohammed")
+    engineers.add("Ahmed")
+    engineers.add("Sherif")
+    engineers.add("Ali")
+    engineers.add("Ashraf")
+    engineers.add("Khaled")
+
+    println("The Engineers in my company are:\n")
+
+    for (i in engineers)
+        println(i)
+
+
+}
