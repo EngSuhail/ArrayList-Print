dee7d38dd03a9c0a7f996fb6bd649d5554888896
EngSuhail <73076012+EngSuhail@users.noreply.github.com>
2020-11-14 13:27:23 +0200

    Another way to print the items of your arraylist

--- src/Suhail.kt
+++ src/Suhail.kt
@@ -2,21 +2,24 @@ fun main() {
 
     val engineers = ArrayList<String>()
 
-    engineers.add("Suhail")
-    engineers.add("Adel")
-    engineers.add("Mostafa")
-    engineers.add("Ibrahim")
-    engineers.add("Mohammed")
-    engineers.add("Ahmed")
-    engineers.add("Sherif")
-    engineers.add("Ali")
-    engineers.add("Ashraf")
-    engineers.add("Khaled")
+    engineers.add("Suhail Alansary")
+    engineers.add("Adel Saad")
+    engineers.add("Mostafa Alnagar")
+    engineers.add("Ibrahim Basem")
+    engineers.add("Mohammed Ahmed")
+    engineers.add("Ahmed Hesham")
+    engineers.add("Sherif Mostafa")
+    engineers.add("Ali Arafat")
+    engineers.add("Ashraf Kamal")
+    engineers.add("Khaled Hamed")
 
     println("The Engineers in my company are:\n")
-
+   //1st method to print the elements
     for (i in engineers)
-        println(i)
+        println("Eng. $i")
 
+   //2nd method to print the elements
+    for (i in 0..engineers.size-1)
+        println("Eng. " + engineers[i])
 
 }
